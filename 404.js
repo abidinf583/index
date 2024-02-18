47175db9789da55703d3dfe4d666b81894020660
abidinf583 <157286342+abidinf583@users.noreply.github.com>
2024-02-18 21:16:31 +0700

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,3 +1,3 @@
 if (window.location.href.includes('?key=')==true) {
-  window.location=window.location.href.replace(window.location.host,'clipperroutesevere.com');
+  window.location=window.location.href.replace(window.location.host+'/','');
 }
